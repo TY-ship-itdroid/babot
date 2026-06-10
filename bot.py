4dfe0fbc8f8af7acc612980da11c70eb9e3e4dd0
TY-ship-itdroid <t1616.066.91@gmail.com>
2026-06-10 20:56:11 +0900

    fix timezone

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def event_notification():
     while not client.is_closed():
         now = datetime.utcnow()  # UTCに変更
         # 日本時間12時 = UTC 3時
-        if now.hour == 11 and now.minute == 55:
+        if now.hour == 12 and now.minute == 00:
             events = get_events()
             if events:
                 message = '**【ブルアカ イベント一覧】**\n'
